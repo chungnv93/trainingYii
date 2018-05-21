25acaa6d7b223965fbfc4eef02b3b360effc96ec
chungnv93 <chungnv@kaopiz.com>
2018-05-21 22:44:03 +0700

    commit packet slug title

--- backend/controllers/CategoryController.php
+++ backend/controllers/CategoryController.php
@@ -3,6 +3,8 @@ namespace backend\controllers;
 use yii\web\Controller;
 use Yii;
 use common\models\Categories;
+use Cocur\Slugify\Slugify;
+
 class CategoryController extends Controller {
     public function actions()
     {
@@ -22,13 +24,12 @@ class CategoryController extends Controller {
         $model = new Categories();
         if($model->load(Yii::$app->request->post()) && $model->validate()) {
             $request = Yii::$app->request->post();
-            
+            $slugify = new Slugify();
             $model->name = $request['Categories']['name'];
-            $model->slug = '123';
+            $model->slug = $slugify->slugify($request['Categories']['name']);
             $model->save();
             Yii::$app->session->setFlash('success', 'Insert Data successfull');
         } 
             return $this->render('/category/create', ['model' => $model]);
-        
     }
 }
